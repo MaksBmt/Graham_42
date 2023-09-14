8fa7f4706073c49cd881fbdf8ec44356a7b2e650
solovev.m <msolov68@gmail.com>
2023-09-14 10:01:04 +0300

    anime.js-htmllab
    https://www.youtube.com/watch?v=2LGZTit4J4I&list=PLOQDek48BpZEr1WUKJ_FNUzlbU57piBT0&index=10

--- src/js/app.js
+++ src/js/app.js
@@ -23,6 +23,7 @@ import OpenMenu from "./openMenu.js";
 import OpenPopup from "./openPopup.js";
 import { correctPaddingTop } from "./modules/functions.js"
 import JSShare from "js-share";
+import anime from 'animejs/lib/anime.es.js';
 
 
 Fancybox.bind("[data-fancybox]", {
@@ -71,6 +72,107 @@ if (document.querySelector('header') !== null) {
     }
   });
 }
+
+//! ===== =====================  anime.js =====================
+const phone = {
+  price: 0,
+  maxPrice: 3000,
+}
+const input = document.querySelector('[name=input1]');
+
+// anime({
+//   targets: input,
+//   value: [0, 1000],
+//   round: 1,
+//   easing: 'easeInOutExpo'
+// });
+
+// anime({
+//   targets: ['.svg-attributes-demo polygon', 'feTurbulence', 'feDisplacementMap'],
+//   points: '64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96',
+//   baseFrequency: 0,
+//   scale: 1,
+//   loop: true,
+//   direction: 'alternate',
+//   easing: 'easeInOutExpo'
+// });
+
+// document.querySelector('.btn').addEventListener('click', () => {
+//   anime({
+//     // targets: ['.range', phone],
+//     targets: ['.el1', '.el2', '.el3', phone],
+//     // loop: true,
+//     // loop: 3,
+//     // backgroundColor: {
+//     //   value: 'rgb(157, 42, 42)',
+//     //   duration: 2000,  //*задержка только для bgc
+//     // },
+//     // translateX: 250,
+//     // translateX: [
+//     //   { value: 250, duration: 1000, delay: 500 },
+//     //   { value: 0, duration: 1000, delay: 500 }
+//     // ],
+
+//     price: 20000,
+//     round: 10,
+//     // delay: 500, //* задержка
+//     // delay: function (el, i, l) { return i * 300; },
+//     // delay: anime.stagger(300),
+//     duration: 2000, //* время выполнения
+//     // rotate: 45,
+//     // rotate: function (el, i, l) {
+//     //   return 10 * i;
+//     // },
+//     // rotate: anime.stagger('1turn'),
+//     // rotate: { value: '+= 1turn' },
+//     easing: 'easeInCubic', //*кривая выполнения
+//     // endDelay: 500,
+//     // direction: 'alternate',
+
+//     update: () => {
+//       document.querySelector('.result').textContent = phone.price;
+//     },
+//     width: '100%', // -> from 'что задано' to '100%',
+//   });
+// });
+
+document.querySelector('.btn').addEventListener('click', () => {
+  // anime({
+  //   targets: ['.range', phone],
+  //   duration: 2000, //* время выполнения
+  //   price: 2000,
+  //   easing: 'easeInCubic', //*кривая выполнения
+  //   // width: '100%',
+  //   update: () => {
+  //     // document.querySelector('.range').style.Width = phone.price / document.querySelector('.control').offsetWidth;
+  //     document.querySelector('.range').style.width = ((document.querySelector('.control').offsetWidth / phone.maxPrice) * phone.price) + 'px';
+
+  //   },
+
+  // });
+
+  anime({
+    targets: '.first path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 5500,
+    delay: function (el, i) { return i * 250 },
+    direction: 'alternate',
+    loop: true
+  });
+});
+
+// document.querySelector('.ceil').addEventListener('click', () => {
+//   anime({
+//     targets: '.cub',
+//     scale: [
+//       { value: .1, easing: 'easeOutSine', duration: 500 },
+//       { value: 1, easing: 'easeInOutQuad', duration: 1200 }
+//     ],
+//     delay: anime.stagger(200, { grid: [14, 5], from: 'center' })
+//   });
+// })
+
 
 
 
